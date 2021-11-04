8951cf1e48fa7565de3858e23d878376c0b27e80
Yegor Bitensky <yegorbitensky@gmail.com>
2021-11-04 23:27:06 +0300

    Add config file

--- node/app/src/index.js
+++ node/app/src/index.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack', '!!!11'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', '!!!12'], ' ');
 
   return element;
 }
