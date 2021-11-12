547f8dd6a2c4319533db7c06b053172ee8f660a7
Yegor Bitensky <yegorbitensky@gmail.com>
2021-11-12 23:54:46 +0300

    Add output management

--- node/app/src/index.js
+++ node/app/src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
 
+import printMe from './print.js';
+
 
 function component() {
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
   const element = document.createElement('div');
-
   element.innerHTML = _.join(['Hello', 'webpack', '!!!12'], ' ');
-
+  element.appendChild(btn);
   return element;
 }
 
