bff40c2277795114bbe27353e42673bc696581d8
Yegor Bitensky <yegorbitensky@gmail.com>
2021-11-19 23:56:53 +0300

    Add entry points

--- node/app/webpack.config.js
+++ node/app/webpack.config.js
@@ -1,27 +1,42 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+// module.exports = {
+//   mode: 'development',
+//   entry: {
+//     index: './src/index.js',
+//     print: './src/print.js',
+//   },
+//   devtool: 'inline-source-map',
+//   devServer: {
+//     static: './dist',
+//     host: '0.0.0.0',
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       title: 'Development',
+//     }),
+//   ],
+//   output: {
+//     filename: '[name].bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     clean: true,
+//     publicPath: '/',
+//   },
+// };
 
 
 module.exports = {
+  entry: './src/index.js',
   mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
+    another: './src/another-module.js',
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-    host: '0.0.0.0',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
   output: {
+    filename: 'main.js',
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
   },
 };
